4bea3be3368e8854c340fbbf65bef8f3ede9c7ae
Lukas Schramm <schramm98@icloud.com>
2015-11-22 15:07:25 +0100

    vereinfachte Syntax in Problem 9
    Eine unsinnige for-Schleife in Problem 9 wurde entfernt.

--- src/problem9/SpecialPythagoreanTriplet.java
+++ src/problem9/SpecialPythagoreanTriplet.java
@@ -5,15 +5,14 @@ public class SpecialPythagoreanTriplet {
 	public static void main(String[] args) {
 		schlc:for(int c=1;c<1000;c++) {
 			for(int b=1;b<c;b++) {
-				for(int a=1;a<b;a++) {
-					if(a*a+b*b==c*c) {
-						if(a+b+c==1000) {
-							System.out.println(a);
-							System.out.println(b);
-							System.out.println(c);
-							System.out.println(a*b*c);
-							break schlc;
-						}
+				int a = 1000 - b -c;
+				if(a*a+b*b==c*c) {
+					if(a+b+c==1000) {
+						System.out.println(a);
+						System.out.println(b);
+						System.out.println(c);
+						System.out.println(a*b*c);
+						break schlc;
 					}
 				}
 			}
